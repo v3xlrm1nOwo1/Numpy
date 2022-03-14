eee651f675616aea75033354085de0148f495507
ブラックハート <87325345+Deve-BlackHeart@users.noreply.github.com>
2022-03-14 09:23:23 +0200

    Update and rename NumPy.py to 0. NumPy.py

--- Tutorial/Code/0. NumPy.py
+++ Tutorial/Code/0. NumPy.py
@@ -1,13 +1,15 @@
-# pip install numpy
-
+# 1. pip install numpy
+# 2. import NumPy 
 import numpy
 
 arr = numpy.array([1,2, 3, 4, 5])
 print(arr)
 
+# 3. import NumPy with Aliases Name
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
+# 4. Version NumPy
 print(np.__version__)
